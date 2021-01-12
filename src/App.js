48d69d4b5e514a9ed0d5436ae077dfff063ae6ab
Manish <2706mkc@gmail.com>
2021-01-12 16:39:37 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './Component/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Login.js
@@ -0,0 +1,46 @@
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+const Login = () => {
+  const onFinish = (values) => {
+    // console.log('Received values of form: ', values);
+    if((values.username === "Admin")  && (values.password === "Admin") ){
+        alert("Valid User")
+    }else{
+        alert("Invalid User")
+    }
+  };
+
+  return (
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: 'Please input your Username!' }]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your Password!' }]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Login
